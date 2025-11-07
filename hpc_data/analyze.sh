3d5bc88023fe4bcfb16cdc7c45ca73e778c92c5f
Anirudha Mirmira <65292350+anirudha-mirmira@users.noreply.github.com>
2025-11-07 13:08:22 -0600

    Change script to use analyze_main.py instead of main_analysis.py

--- hpc_data/analyze.sh
+++ hpc_data/analyze.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+source ~/alps-venv/bin/activate
 
 # Loop through each dca_tp.hdf5 file in directories starting with T
 for file in T*/dca_tp.hdf5; do
     if [[ -f "$file" ]]; then  # Check if the file exists
         echo "Processing $file..."
-        python main_analysis.py "$file"  # Run the Python script on each file
+        python analyze_main.py "$file"  # Run the Python script on each file
     else
         echo "File $file does not exist, skipping."
     fi
